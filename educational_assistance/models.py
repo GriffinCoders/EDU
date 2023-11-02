ff8a170ad0e019a8f84343646e2d5eaab1f8866e
Fazel <fazelsamar@gmail.com>
2023-11-03 01:39:20 +0330

    feat: add created_at and updated_at to all models

--- educational_assistance/models.py
+++ educational_assistance/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 from account.models import User
-from common.models import College, Field
+from common.models import College, Field, BaseModel
 
 
-class EducationalAssistanceProfile(models.Model):
+class EducationalAssistanceProfile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
     field = models.ForeignKey(Field, on_delete=models.CASCADE)
