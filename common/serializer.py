e658b003ba10a3980e3504caa73ef912b801709f
Yegane Gholipour <yeganegholiour@gmail.com>
2023-11-03 11:45:13 +0330

    feat: Add serializers for common/College and Field models

---
+++ common/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import College, Field
+
+class CollegeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = College
+        fields = ['name', 'id', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+    
+class FieldSerializer(serializers.ModelSerializer):
+    college = CollegeSerializer()
+
+    class Meta:
+        model = Field
+        fields = ['id', 'name', 'educational_group', 'college', 'units', 'grade', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
