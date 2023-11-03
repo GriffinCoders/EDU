274ff02cc7f9055c3cf7731f69867e6a09421f76
Fazel <fazelsamar@gmail.com>
2023-11-03 17:14:17 +0330

    feat: add assistance crud for student and professor

---
+++ educational_assistance/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+from account.models import UserRoleChoices
+from educational_assistance.models import EducationalAssistanceProfile
+
+
+class IsEducationalAssistance(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.role == UserRoleChoices.EducationalAssistance and
+            EducationalAssistanceProfile.objects.filter(user=request.user).exists()
+        )
--- educational_assistance/views.py
+++ educational_assistance/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from professor.models import ProfessorProfile
+from student.models import StudentProfile
+from . import serializers
+from .models import EducationalAssistanceProfile
+from .permissions import IsEducationalAssistance
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.StudentSerializer
+    permission_classes = [IsAuthenticated, IsEducationalAssistance]
+    http_method_names = ['get', 'put']
+
+    def get_queryset(self):
+        college_id = EducationalAssistanceProfile.objects.get(user=self.request.user).college_id
+        return StudentProfile.objects.filter(college_id=college_id).select_related(
+            'user', 'entry_term', 'college', 'field', 'supervisor',
+        )
+
+
+class ProfessorViewSet(StudentViewSet):
+    serializer_class = serializers.ProfessorSerializer
+
+    def get_queryset(self):
+        college_id = EducationalAssistanceProfile.objects.get(user=self.request.user).college_id
+        return ProfessorProfile.objects.filter(college_id=college_id).select_related(
+            'user', 'college', 'field',
+        )
