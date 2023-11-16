476701ffcdd1bef97b0296535cff29888199e1c9
shahramtrb <shahramtrb@gmail.com>
2023-11-16 23:32:28 +0330

    course serializer modified

--- course/serializers.py
+++ course/serializers.py
@@ -27,28 +27,12 @@ class CourseSerializer(serializers.ModelSerializer):
 
     # validate method to check if substitution time is finished or not
     def validate(self, attrs):
-        # check the duplication term for the student
+        # Check the substitution time
         term: Term = attrs.get('term')
-        student: StudentProfile = self.context['student_obj']
-        if CourseSelectionRequest.objects.filter(student=student, term=term).exists():
-            raise serializers.ValidationError("The Course Selection for this term already exists")
-
-        # Check the selection time
+        
         now = timezone.now()
-        if term.substitution_start > now:
-            raise serializers.ValidationError("The Course Selection not started")
-        if term.substitution_finish < now:
-            raise serializers.ValidationError("The Course Selection time has passed")
-
-        # Check the valid years
-        if not CourseSelectionRequest.check_student_valid_years(student):
-            raise serializers.ValidationError("Not enough valid years")
-
-        # Check the last term gpa
-        attrs['valid_unit'] = 20
-        last_gpa = CourseSelectionRequest.get_student_last_gpa(student)
-        if last_gpa and last_gpa >= 17:
-            attrs['valid_unit'] = 24
+        if term.substitution_finish < now and term.term_finish > now:
+            raise serializers.ValidationError("The Course Substitution is not possible")
 
         return attrs
          
