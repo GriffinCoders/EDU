0e0c465a9d51e38290c69b2a23f5d39c33d957a3
Fazel <fazelsamar@gmail.com>
2023-11-17 22:43:27 +0330

    fix: add session authentication

--- EDU/settings.py
+++ EDU/settings.py
@@ -166,8 +166,8 @@ AUTH_USER_MODEL = 'account.User'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        # "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
